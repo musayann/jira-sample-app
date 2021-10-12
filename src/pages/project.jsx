80a3f4b5c40ac94e6282a2f223303ccb0e943d31
musayann <musayann@octangrp.com>
2021-10-12 16:00:34 +0200

    feat: add board columns

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -5,8 +5,9 @@ import JIRA from "@atlassian/jira";
 function Home() {
   const [accessToken, setAccessToken] = useState(undefined);
   const [project, setProject] = useState(undefined);
+  const [columns, setColumns] = useState({});
   const history = useHistory();
-  const {cloudId, id} = useParams();
+  const { cloudId, id } = useParams();
 
   useEffect(() => {
     const access_token = localStorage.getItem("access_token");
@@ -19,7 +20,7 @@ function Home() {
       const myHeaders = new Headers();
       myHeaders.append("Authorization", `Bearer ${access_token}`);
 
-     const jira = new JIRA({
+      const jira = new JIRA({
         baseUrl: `https://api.atlassian.com/ex/jira/${cloudId}/rest/`,
         headers: {},
         options: {
@@ -33,27 +34,81 @@ function Home() {
       });
 
       const result = await jira.project.getProject({
-        projectIdOrKey: id || '',
-        expand: 'lead,issueTypes'
-      })
-    // const result = await jira.project.
-      console.log(result.data);
-    //   setProject(result.data);
+        projectIdOrKey: id || "",
+        expand: "lead,issueTypes",
+      });
+      const proj = result.data;
+      setProject(proj);
+      const response = await jira.statuscategory.getAllStatusCategories();
+      const cols = {};
+      response.data
+        .filter((col) => col.key !== "undefined")
+        .forEach((col) => {
+          cols[col.key] = {
+            ...col,
+            issues: [],
+          };
+        });
+        const { data } = await jira.search.searchForIssuesUsingJqlPost({
+          body: {
+            jql: `project = '${proj.key}' ORDER BY created DESC`,
+          },
+        });
+      data.issues.forEach((issue)=> {
+          cols[issue.fields.status.statusCategory.key].issues.push(issue);
+      });
+      setColumns(cols);
+
+      // const workflows = await jira.workflow.getAllWorkflows();
+      // console.log(workflows);
     })();
   }, [cloudId, id]);
 
   return (
     <div className="bg-gray-800 min-h-screen text-white">
       <div className="w-4/5 py-16 mx-auto">
-        <h1 className="uppercase mb-4 text-3xl">Projects</h1>
-        <ul className="text-center w-full">
-          <li className="w-full flex items-center px-5 py-2 bg-gray-900 rounded-md my-3 text-left uppercase">
-            <div className="w-16" />
-            <div className="w-2/4">Name</div>
-            <div>Key</div>
-          </li>
-          
-        </ul>
+        <h1 className="text-3xl">{project?.name}</h1>
+        <span className="text-xs opacity-50">{project?.key}</span>
+        <div className="w-full flex space-x-5 mt-4">
+          {Object.keys(columns).map((key, i) => {
+            const column = columns[key];
+            return (
+              <div className="w-1/3 flex flex-col space-y-5">
+                <div className="text-right text-sm">{column.name}</div>
+                {column.issues.map((issue) => (
+                  <div
+                    key={issue.id}
+                    className="px-5 py-6 bg-gray-700 rounded-xl shadow-md"
+                  >
+                    <div className="text-base">{issue.fields.summary}</div>
+                    <div className="text-xs bg-red-500 mt-3 inline-block rounded px-2">
+                      {issue.fields.issuetype.name}
+                    </div>
+                    <div className="flex justify-between pt-4 items-center">
+                      <div className="flex items-center text-sm">
+                        {issue.fields.assignee && (
+                          <>
+                            <div className="pr-2">
+                              <img
+                                src={issue.fields.assignee.avatarUrls["48x48"]}
+                                className="h-6 w-6 rounded-full"
+                                alt={issue.fields.assignee.displayName}
+                              />
+                            </div>
+                            <div>{issue.fields.assignee.displayName}</div>
+                          </>
+                        )}
+                      </div>
+                      <div className="text-right text-xs opacity-50">
+                        {issue.key}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
